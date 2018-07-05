3e7b0cd7c47b06a8cbd34714a891c021811c25cb
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-05 02:08:32 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  lowest_number= nil 
+  lowest_key=nil
+name_hash.collect do |key, int|
+  if lowest_number==nil
+    lowest_number=int
+    lowest_key=key
+  end
+    if int< lowest_number
+    lowest_key=key
+    lowest_number=int
+  end
+   return lowest_key
+ 
+end
+  
 end
