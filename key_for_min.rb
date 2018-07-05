c79cb807f82d0d3e0f53a49f24ba8b28757250c9
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-05 02:09:35 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,16 +5,10 @@ def key_for_min_value(name_hash)
   lowest_number= nil 
   lowest_key=nil
 name_hash.collect do |key, int|
-  if lowest_number==nil
+  if lowest_number==nil || int<lowest_number 
     lowest_number=int
     lowest_key=key
-  end
-    if int< lowest_number
-    lowest_key=key
-    lowest_number=int
   end
    return lowest_key
- 
-end
   
 end
